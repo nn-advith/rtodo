985031410e31eb025ab37de66682a5b8f733555d
N N Advith <nnadvith08042001@gmail.com>
2024-08-27 00:57:47 +0530

    Design

--- App.js
+++ App.js
@@ -8,11 +8,14 @@ import {
   View,
   Pressable,
   TextInput,
+  Dimensions,
+  ScrollView,
 } from "react-native";
 import { SQLiteProvider, useSQLiteContext } from "expo-sqlite";
 import { useState, useEffect } from "react";
 import DateTimePicker from "@react-native-community/datetimepicker";
 import moment from "moment";
+import { getStatusBarHeight } from "react-native-status-bar-height";
 
 async function initializeDB(db) {
   try {
@@ -32,20 +35,34 @@ async function initializeDB(db) {
 
 export default function App() {
   const [dateToday, setDateToday] = useState("");
+  const [dayToday, setDayToday] = useState("");
+  const [dd, setDD] = useState("01");
+  const [mm, setMM] = useState("01");
   useEffect(() => {
+    const date = new Date();
+
     setDateToday(
-      new Date().toISOString().split("T")[0].split("-").reverse().join("-")
+      date.toISOString().split("T")[0].split("-").reverse().join("-")
     );
+    setDD(date.getDate());
+    setMM(date.getMonth() + 1);
+    setDayToday(date.toLocaleDateString("en-US", { weekday: "long" }));
   }, []);
 
   return (
     <SQLiteProvider databaseName="notes.db" onInit={initializeDB}>
       <View style={styles.container}>
-        <Text style={styles.title}>Notes</Text>
-        <Text style={styles.date}>{dateToday}</Text>
+        <View style={styles.banner}>
+          <Text style={styles.text2}>
+            {dd}
+            <Text style={{ color: "#ff3333" }}>/</Text>
+            {mm}
+          </Text>
+          <Text style={styles.text1}>{dayToday}</Text>
+        </View>
         <Content style={styles.content} dateToday={dateToday} />
-        <StatusBar style="light" />
       </View>
+      <StatusBar style="light" />
     </SQLiteProvider>
   );
 }
@@ -106,9 +123,7 @@ const NoteForm = ({
     setShowCalendar(!showCalendar);
   };
 
-  useEffect(() => {
-    console.log(dateString);
-  }, [dateString]);
+  useEffect(() => {}, [dateString]);
 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
@@ -170,16 +185,14 @@ const NoteItem = ({ item, handleDelete, type, setNote }) => {
         {" "}
         {item.note} - {item.due}
       </Text>
-      <Pressable
+      {/* <Pressable
         onPress={() => setNote(item)}
         style={styles.editbutton}
-      ></Pressable>
+      ></Pressable> */}
       <TouchableOpacity
         onPress={() => handleDelete(item.id, type)}
         style={styles.button}
-      >
-        <Text>Delete</Text>
-      </TouchableOpacity>
+      ></TouchableOpacity>
     </View>
   );
 };
@@ -206,14 +219,13 @@ const Content = ({ dateToday }) => {
 
   const categorizeNotes = () => {
     try {
-      const todayDate = new Date(dateToday).getTime();
+      const todayDate = moment(dateToday, "DD-MM-YYYY").valueOf();
 
       for (i = 0; i < notes.length; i++) {
-        const noteDate = new Date(notes[i].due).getTime();
+        const noteDate = moment(notes[i].due, "DD-MM-YYYY").valueOf();
         if (noteDate === null) {
           setOTasks((oTasks) => [...oTasks, notes[i]]);
         }
-
         if (todayDate < noteDate) {
           if (!oTasks.some((k) => k.id === notes[i].id)) {
             setOTasks([...oTasks, notes[i]]);
@@ -264,19 +276,18 @@ const Content = ({ dateToday }) => {
   };
 
   const handleDelete = (id, section) => {
-    console.log(section);
     try {
       if (section === "c") {
         index = cTasks.findIndex((k) => k.id === id);
-        console.log(index);
+
         setCTasks([...cTasks.slice(0, index), ...cTasks.slice(index + 1)]);
       } else if (section === "t") {
         index = tTasks.findIndex((k) => k.id === id);
-        console.log(index);
+
         setTTasks([...tTasks.slice(0, index), ...tTasks.slice(index + 1)]);
       } else if (section === "o") {
         index = oTasks.findIndex((k) => k.id === id);
-        console.log(index);
+
         setOTasks([...oTasks.slice(0, index), ...oTasks.slice(index + 1)]);
       } else {
         throw new Error("Doesn't match type of note");
@@ -292,12 +303,25 @@ const Content = ({ dateToday }) => {
   };
 
   useEffect(() => {
+    // addNote({ note: "TASK", due: "31-03-2024" });
+    // addNote({ note: "TASK", due: "31-03-2024" });
+    // addNote({ note: "TASK", due: "31-03-2024" });
+    // addNote({ note: "TASK", due: "31-10-2024" });
+    // addNote({ note: "TASK", due: "31-10-2024" });
+    // addNote({ note: "TASK", due: "31-10-2024" });
+    // addNote({ note: "TASK", due: "31-03-2024" });
+    // addNote({ note: "TASK", due: "31-03-2024" });
+    // addNote({ note: "TASK", due: "31-03-2024" });
+    // addNote({ note: "TASK", due: "31-10-2024" });
+    // addNote({ note: "TASK", due: "31-10-2024" });
+    // addNote({ note: "TASK", due: "31-10-2024" });
+
+    // deleteAllNotes();
     getNotes();
   }, []);
 
   useEffect(() => {
     categorizeNotes();
-    console.log("Notes updated");
   }, [notes]);
 
   // useEffect(() => {}, [cTasks, oTasks, tTasks]);
@@ -307,84 +331,107 @@ const Content = ({ dateToday }) => {
   }, [note]);
 
   return (
-    <View style={{ flex: 1 }}>
-      {cTasks.length === 0 ? (
-        <View></View>
-      ) : (
-        <View style={{ flex: 5 }}>
-          <View style={styles.header}>
-            <Text style={styles.text}>Carried Over</Text>
-          </View>
-          <FlatList
-            data={cTasks}
-            renderItem={({ item }) => (
-              <NoteItem
-                item={item}
-                handleDelete={handleDelete}
-                type="c"
-                setNote={setNote}
+    <View
+      style={{
+        flex: 15,
+        width: Dimensions.get("window").width,
+        paddingTop: 20,
+      }}
+    >
+      <View style={{ flex: 10, padding: 0, margin: 0 }}>
+        <ScrollView
+          style={{ flex: 1, overflow: "visible" }}
+          horizontal={false}
+          removeClippedSubviews={false}
+        >
+          {cTasks.length === 0 ? (
+            <View></View>
+          ) : (
+            <View style={{ borderLeftColor: "#ff3333", borderLeftWidth: 1 }}>
+              <View style={styles.header}>
+                <Text style={styles.text3}>Pending</Text>
+              </View>
+              <FlatList
+                data={cTasks}
+                renderItem={({ item }) => (
+                  <NoteItem
+                    item={item}
+                    handleDelete={handleDelete}
+                    type="c"
+                    setNote={setNote}
+                  />
+                )}
+                keyExtractor={(item) => item.id.toString()}
+                horizontal={false}
+                scrollEnabled={false}
+                showsVerticalScrollIndicator={false}
               />
-            )}
-            keyExtractor={(item) => item.id.toString()}
-          />
-        </View>
-      )}
+            </View>
+          )}
 
-      {tTasks.length === 0 ? (
-        <View></View>
-      ) : (
-        <View style={{ flex: 5 }}>
-          <View style={styles.header}>
-            <Text style={styles.text}>Today's tasks</Text>
-          </View>
-          <FlatList
-            data={tTasks}
-            renderItem={({ item }) => (
-              <NoteItem
-                item={item}
-                handleDelete={handleDelete}
-                type="t"
-                setNote={setNote}
+          {tTasks.length === 0 ? (
+            <View></View>
+          ) : (
+            <View style={{ borderLeftColor: "#33A9FF", borderLeftWidth: 2 }}>
+              <View style={styles.header}>
+                <Text style={styles.text3}>Today</Text>
+              </View>
+              <FlatList
+                data={tTasks}
+                renderItem={({ item }) => (
+                  <NoteItem
+                    item={item}
+                    handleDelete={handleDelete}
+                    type="t"
+                    setNote={setNote}
+                  />
+                )}
+                keyExtractor={(item) => item.id.toString()}
+                horizontal={false}
+                scrollEnabled={false}
+                showsVerticalScrollIndicator={false}
               />
-            )}
-            keyExtractor={(item) => item.id.toString()}
-          />
-        </View>
-      )}
+            </View>
+          )}
 
-      {oTasks.length === 0 ? (
-        <View></View>
-      ) : (
-        <View style={{ flex: 5 }}>
-          <View style={styles.header}>
-            <Text style={styles.text}>Other tasks</Text>
-          </View>
-          <FlatList
-            data={oTasks}
-            renderItem={({ item }) => (
-              <NoteItem
-                item={item}
-                handleDelete={handleDelete}
-                type="o"
-                setNote={setNote}
+          {oTasks.length === 0 ? (
+            <View></View>
+          ) : (
+            <View style={{ borderLeftColor: "#2CDD00", borderLeftWidth: 1 }}>
+              <View style={styles.header}>
+                <Text style={styles.text3}>Other</Text>
+              </View>
+              <FlatList
+                data={oTasks}
+                renderItem={({ item }) => (
+                  <NoteItem
+                    item={item}
+                    handleDelete={handleDelete}
+                    type="o"
+                    setNote={setNote}
+                  />
+                )}
+                keyExtractor={(item) => item.id.toString()}
+                horizontal={false}
+                scrollEnabled={false}
+                showsVerticalScrollIndicator={false}
               />
-            )}
-            keyExtractor={(item) => item.id.toString()}
-          />
-        </View>
-      )}
+            </View>
+          )}
 
-      {showForm && (
-        <NoteForm
-          dateToday={dateToday}
-          showForm={showForm}
-          setShowForm={setShowForm}
-          note={note}
-          setNote={setNote}
-          addNote={addNote}
-        />
-      )}
-      <View style={{ flex: 2, backgroundColor: "blue" }}>
+          {showForm && (
+            <NoteForm
+              dateToday={dateToday}
+              showForm={showForm}
+              setShowForm={setShowForm}
+              note={note}
+              setNote={setNote}
+              addNote={addNote}
+            />
+          )}
+        </ScrollView>
+      </View>
+      <View style={{ flex: 1 }}>
         <TouchableOpacity
           onPress={handleAdd}
           style={styles.button}
@@ -400,17 +447,43 @@ const Content = ({ dateToday }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#111",
+    backgroundColor: "#0a0a0a",
     alignItems: "flex-start",
     justifyContent: "flex-start",
-    paddingTop: 50,
+    paddingTop: getStatusBarHeight(),
     paddingLeft: 10,
+  },
+  banner: {
+    flex: 1,
+    flexDirection: "row",
+    paddingHorizontal: 7,
+    backgroundColor: "#0a0a0a",
+    width: Dimensions.get("window").width,
+    marginTop: 7,
+  },
+  text1: {
+    fontWeight: "800",
+    color: "#ff3333",
+    fontSize: 34,
+    marginLeft: 7,
+  },
+  text2: {
+    fontWeight: "600",
+    color: "#fff",
+    fontSize: 22,
+    paddingTop: 10,
+    marginLeft: 10,
+  },
+  text3: {
+    fontWeight: "400",
+    color: "#9F9F9F",
+    fontSize: 18,
+    paddingLeft: 12,
   },
   title: {
     fontSize: 28,
     color: "#FFF",
     fontWeight: "bold",
-    marginBottom: 40,
   },
   text: {
     color: "#FFF",
@@ -421,7 +494,8 @@ const styles = StyleSheet.create({
     display: "flex",
     flexDirection: "row",
     justifyContent: "space-between",
-    width: "100%",
+    width: "90%",
+    paddingLeft: 7,
   },
   button: {
     color: "#000",
@@ -457,10 +531,12 @@ const styles = StyleSheet.create({
     borderRadius: 5,
   },
   header: {
-    fontWeight: "700",
-    width: 150,
-    padding: 5,
-    backgroundColor: "#00ff00",
+    fontWeight: "500",
+    paddingHorizontal: 0,
+    color: "#9F9F9F",
+    // width: Dimensions.get("window").width,
+    marginBottom: 15,
+    flexDirection: "row",
   },
   calendar: {
     width: 30,
